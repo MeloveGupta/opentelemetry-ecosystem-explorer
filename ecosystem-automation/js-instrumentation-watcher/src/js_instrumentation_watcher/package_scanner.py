40a75971115f9b69ff7832aaef3d9356ee9f9e45
Melove <melovegupta0406@gmail.com>
2026-05-24 04:16:26 +0530

    chore: add missing copyright headers to js-instrumentation-watcher

--- ecosystem-automation/js-instrumentation-watcher/src/js_instrumentation_watcher/package_scanner.py
+++ ecosystem-automation/js-instrumentation-watcher/src/js_instrumentation_watcher/package_scanner.py
@@ -11,6 +11,7 @@
 # WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 # See the License for the specific language governing permissions and
 # limitations under the License.
+#
 
 """Scanner for JS instrumentation packages in the js-contrib repository."""
 
